39e6140ff6b490ab85f95d9fd98d99f41fa4b322
hmjn023 <hmjn023@gmail.com>
2023-08-10 22:25:37 +0900

    fix file construct

---
+++ src/main/kotlin/hmjn023/protoutils/common/block/TestBlock.kt
@@ -0,0 +1,13 @@
+package hmjn023.protoutils.block
+
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties
+
+
+class TestBlock(properties: Properties) :Block(properties){
+    init {
+
+    }
+}
+
+
---
+++ src/main/kotlin/hmjn023/protoutils/common/block/hoge.java
@@ -0,0 +1,9 @@
+package hmjn023.protoutils.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class hoge extends Block {
+    public hoge(Properties properties) {
+        super(properties);
+    }
+}
