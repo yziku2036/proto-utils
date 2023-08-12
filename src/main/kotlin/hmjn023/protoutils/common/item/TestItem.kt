2013862080e0a8964eb154e52aac65174cfa27fc
yziku2036 <yziku2036@gmail.com>
2023-08-12 22:32:39 +0900

    Fixed Lightning Staff Name Case

--- src/main/kotlin/hmjn023/protoutils/common/item/TestItem.kt
+++ src/main/kotlin/hmjn023/protoutils/common/item/TestItem.kt
@@ -1,23 +1,7 @@
 package hmjn023.protoutils.common.item
 
-import hmjn023.protoutils.ProtoUtils
-import net.minecraft.core.BlockPos
-import net.minecraft.nbt.Tag
-import net.minecraft.nbt.NbtUtils
-import net.minecraft.world.InteractionHand
-import net.minecraft.world.InteractionResult
-import net.minecraft.world.InteractionResultHolder
-import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.context.BlockPlaceContext
-import net.minecraft.world.item.context.UseOnContext
-import net.minecraft.world.level.ClipContext
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.nbt.CompoundTag;
-    class TestItem(properties: Properties?) : Item(properties) {
+
+class TestItem(properties: Properties?) : Item(properties) {
 
     }
