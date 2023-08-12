b600b761610e9a8350d8ec87a9f8f28b3e2d7662
yziku2036 <yziku2036@gmail.com>
2023-08-12 22:19:22 +0900

    hoge

--- src/main/kotlin/hmjn023/protoutils/common/item/TestItem.kt
+++ src/main/kotlin/hmjn023/protoutils/common/item/TestItem.kt
@@ -19,4 +19,5 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.nbt.CompoundTag;
     class TestItem(properties: Properties?) : Item(properties) {
+
     }
